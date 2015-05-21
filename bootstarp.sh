614744b00b2ca780293883e1bb3e8f93fe5d7368
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-21 16:48:56 +0100

    New WIP branch, use build subdir and do a full clone

--- bootstarp.sh
+++ bootstarp.sh
@@ -4,9 +4,9 @@ DOCKER_FORK=${DOCKER_FORK:-"https://github.com/squaremo/docker"}
 DOCKER_FORK_BRANCH=${DOCKER_FORK_BRANCH:-"network_extensions"}
 
 WEAVE_FORK=${WEAVE_FORK:-"https://github.com/squaremo/weave"}
-WEAVE_FORK_BRANCH=${WEAVE_FORK_BRANCH:-"plugin_ipam"}
+WEAVE_FORK_BRANCH=${WEAVE_FORK_BRANCH:-"libnetwork_plugin"}
 
-git clone --depth=10 --branch=$DOCKER_FORK_BRANCH $DOCKER_FORK docker
-git clone --depth=10 --branch=$WEAVE_FORK_BRANCH $WEAVE_FORK weave
+git clone --branch=$DOCKER_FORK_BRANCH $DOCKER_FORK .build/docker
+git clone --branch=$WEAVE_FORK_BRANCH $WEAVE_FORK .build/weave
 
 vagrant up builder
