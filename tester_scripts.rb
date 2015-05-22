737d2179a74d5650869f6000dbf888d2758717fc
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-05-22 16:28:48 +0100

    Install ZFS packages

--- tester_scripts.rb
+++ tester_scripts.rb
@@ -2,8 +2,9 @@ $install_docker = <<SCRIPT
 cp /vagrant/.build/docker/bundles/1.7.0-dev/binary/docker-1.7.0-dev /usr/bin/docker
 cp /vagrant/.build/weave/weave /usr/bin/
 export DEBIAN_FRONTEND=noninteractive
+add-apt-repository -y ppa:zfs-native/stable
 apt-get -qq update
-apt-get -qq install cgroupfs-mount cgroup-lite xz-utils git
+apt-get -qq install cgroupfs-mount cgroup-lite xz-utils git libc6-dev zfsutils
 groupadd docker
 usermod -a -G docker vagrant
 cp /vagrant/docker.conf /etc/init/
