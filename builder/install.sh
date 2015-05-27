6d08688c54da43fc45464491427cb2f1db99687f
Kai Davenport <kaiyadavenport@gmail.com>
2015-05-27 15:40:28 +0100

    needed to stop docker.io before replacing the docker binary

--- builder/install.sh
+++ builder/install.sh
@@ -3,12 +3,17 @@
 # this is the second stage where we install everything onto
 # a running Amazon instance
 export DEBIAN_FRONTEND=noninteractive
+set -e
 
 # vars
-FLOCKER_ZPOOL_SIZE = '10G'
-FLOCKER_PREFIX = '/var/opt/flocker'
+FLOCKER_ZPOOL_SIZE='10G'
+FLOCKER_PREFIX='/var/opt/flocker'
+
+DOCKER_NAME="1.7.0-dev-experimental"
 
 BASE_DEB_FOLDER="http://build.clusterhq.com/results/omnibus/storage-driver-configuration-FLOC-1925/ubuntu-14.04"
+CGROUPSFS_FOLDER="http://ftp.uk.debian.org/debian/pool/main/c/cgroupfs-mount"
+CGROUPSFS_BINARY="cgroupfs-mount_1.2_all.deb"
 
 # deps
 add-apt-repository -y ppa:zfs-native/stable
@@ -19,11 +24,13 @@ apt-get -qq update
 apt-get install -y \
   apt-transport-https \
   software-properties-common \
-  cgroupfs-mount \
   cgroup-lite \
   xz-utils \
   libc6-dev \
   zfsutils
+
+# there is no package for cgroupfs-mount on Ubuntu 14.04 so we install manually
+cd ~ && wget $CGROUPSFS_FOLDER/$CGROUPSFS_BINARY && dpkg -i $CGROUPSFS_BINARY
 
 # zpool
 mkdir -p $FLOCKER_PREFIX
@@ -33,18 +40,19 @@ zpool create flocker $FLOCKER_PREFIX/pool-vdev
 # install flocker
 apt-get -y --force-yes install clusterhq-flocker-node clusterhq-flocker-cli
 
-# copy docker/weave binaries
-cp /vagrant/.build/docker/bundles/1.7.0-dev-experimental/binary/docker-1.7.0-dev-experimental /usr/bin/docker
+# copy weave script
 cp /vagrant/.build/weave/weave /usr/bin/
 
 # setup docker
-groupadd docker
+service docker.io stop
+cp /vagrant/.build/docker/bundles/$DOCKER_NAME/binary/docker-$DOCKER_NAME /usr/bin/docker
+groupadd docker || true
 usermod -a -G docker vagrant
 cp /vagrant/docker.conf /etc/init/
 start docker
 sleep 2
 
-# import docker images
+# import docker images (created by the compiler)
 for i in /vagrant/.build/weave/*.tar
 do docker load -i $i
 done
