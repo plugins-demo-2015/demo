f45b5215fd879678b82e81d6020e3430699badb1
Kai Davenport <kaiyadavenport@gmail.com>
2015-05-28 13:53:34 +0100

    get IP addresses of nodes once vagrant up has completed

--- amibuilder/build.sh
+++ amibuilder/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 vagrant up ami --provider=aws
-instanceid=$(vagrant awsinfo -m ami -p | jq '.instance_id')
-amiid=$(aws ec2 create-image --instance-id $instanceid --name docker-plugins-demo | jq '.ImageId')
+instanceid=$(vagrant awsinfo -m ami -p | jq -r '.instance_id')
+amiid=$(aws ec2 create-image --instance-id $instanceid --name docker-plugins-demo | jq -r '.ImageId')
 echo "AMI image has been created"
 echo $amiid
--- runner/run.sh
+++ runner/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 vagrant up
+masterip=$(vagrant awsinfo -m master -p | jq -r '.private_ip')
+runner1ip=$(vagrant awsinfo -m runner-1 -p | jq -r '.private_ip')
+runner2ip=$(vagrant awsinfo -m runner-2 -p | jq -r '.private_ip')
