03e2b150fba532f0e6e26fb56a11b476a9712cf1
Kai Davenport <kaiyadavenport@gmail.com>
2015-05-28 14:07:39 +0100

    use sensible names for the instances

--- aws_credentials.rb
+++ aws_credentials.rb
@@ -8,11 +8,7 @@ def inject_aws_credentials(aws, override, amifield)
   aws.access_key_id = aws_config.fetch("access_key_id")
   aws.secret_access_key = aws_config.fetch("secret_access_key")
   aws.keypair_name = aws_config.fetch("keypair_name")
-  name = aws_config.fetch("instance_name_prefix") + " docker-plugins-" + amifield
-  aws.tags = {
-      'Name' => name
-  }
-
+  
   # this AMI is specific to the EAST 1 region - the keypair must also be in that region
   aws.ami = aws_config.fetch(amifield)#{}"ami-3cf8b154"
   aws.instance_type = aws_config.fetch("instance_type")#{}"c3.xlarge"
@@ -21,4 +17,12 @@ def inject_aws_credentials(aws, override, amifield)
   override.vm.box = "https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box"
   override.ssh.username = "ubuntu"
   override.ssh.private_key_path = aws_config.fetch("keypair_path")
+end
+
+def inject_aws_instance_name(aws, nodename)
+  aws_config = get_aws_credentials()
+  name = aws_config.fetch("instance_name_prefix") + " docker-plugins " + nodename
+  aws.tags = {
+      'Name' => name
+  }
 end
