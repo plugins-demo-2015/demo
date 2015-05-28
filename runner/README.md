b11148a58d5625fd23808dd9318cc8fc8b70ce69
Kai Davenport <kaiyadavenport@gmail.com>
2015-05-28 18:21:53 +0100

    start the configure Flocker task using unofficial-flocker-tools

--- runner/README.md
+++ runner/README.md
@@ -8,7 +8,7 @@ You will need the `flocker-ca` tool installed on your machine.  To install on OS
 $ brew tap ClusterHQ/tap
 $ brew update
 $ brew install flocker-0.4.1dev1
-$ brew test flocker-0.4.1dev1
+$ flocker-ca --version
 ```
 
 For [other platforms](http://doc-dev.clusterhq.com/using/installing/index.html#installing-flocker-cli)
--- runner/run.sh
+++ runner/run.sh
@@ -23,6 +23,35 @@ function start-weave-plugin() {
     -socket=/usr/share/docker/plugins/weave.sock $peers"
 }
 
+# clone https://github.com/lukemarsden/unofficial-flocker-tools
+# generate cluster.yml
+# run deploy.py
+function configure-flocker() {
+  git clone https://github.com/lukemarsden/unofficial-flocker-tools
+  cat << EOF > unofficial-flocker-tools/cluster.yml
+cluster_name: name 
+agent_nodes:
+ - $runner1ip
+ - $runner2ip
+control_node: $masterip
+users:
+ - flockerdemo
+os: ubuntu
+private_key_path: ~/.ssh/kai-demo.pem
+agent_config:
+  version: 1
+  control-service:
+     hostname: $masterip
+     port: 4524
+  dataset:
+    backend: "aws"
+    region: "us-east-1"
+    zone: "us-east-1c"
+    access_key_id: $awsid
+    secret_access_key: $awskey
+EOF
+}
+
 # bring up the cluster
 vagrant up --parallel
 
