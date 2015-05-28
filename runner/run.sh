f7ff3ecfc03802fa427738139d5606eb9c0ae3ff
Kai Davenport <kaiyadavenport@gmail.com>
2015-05-28 14:20:13 +0100

    script to spin up weave plugin on nodes

--- runner/run.sh
+++ runner/run.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
 
+# return the private IP of a named node
+function get-node-private-ip() {
+  local node="$1"; shift;
+  vagrant awsinfo -m $node -p | jq -r '.private_ip'
+}
+
+# run the weave plugin on a named node
+# the peer IP addresses are passed as args
+function start-weave-plugin() {
+  local node="$1"; shift;
+  local peers="$@";
+
+  vagrant ssh $node -c "sudo docker run -d \
+    --name=weaveplugin \
+    --privileged \
+    --net=host \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v /usr/share/docker/plugins:/usr/share/docker/plugins \
+    weaveworks/plugin \
+    -debug=true \
+    -socket=/usr/share/docker/plugins/weave.so $peers"
+}
+
+# bring up the cluster
 vagrant up
-masterip=$(vagrant awsinfo -m master -p | jq -r '.private_ip')
-runner1ip=$(vagrant awsinfo -m runner-1 -p | jq -r '.private_ip')
-runner2ip=$(vagrant awsinfo -m runner-2 -p | jq -r '.private_ip')
+
+# get the IP addresses of the nodes
+masterip=$(get-node-private-ip master)
+runner1ip=$(get-node-private-ip runner-1)
+runner2ip=$(get-node-private-ip runner-2)
+
+# kick off the weave plugin on each node
+start-weave-plugin master
+start-weave-plugin runner-1 $masterip $runner2ip
+start-weave-plugin runner-2 $masterip $runner1ip
