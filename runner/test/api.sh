2719d7cdcea111e19289c5e38c2f0d7b99306045
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-09 10:34:29 +0100

    script for manual testing

--- runner/test/api.sh
+++ runner/test/api.sh
@@ -6,35 +6,43 @@ set -e
 KEY_FILE=/etc/flocker/plugin.key
 CERT_FILE=/etc/flocker/plugin.crt
 CA_FILE=/etc/flocker/cluster.crt
-CONTROL_SERVICE=54.204.134.124 # 10.229.91.29
-RUNNER_1=54.163.111.97 # 10.71.204.72
-RUNNER_2=54.158.107.49 # 10.142.8.207
+CONTROL_SERVICE=54.158.167.229 # 10.143.219.242
+RUNNER_1=54.145.174.255 # 10.152.24.122
+RUNNER_2=54.144.197.228 # 10.111.199.253
 
 # where to initially create the volume
-START_NODE=928beef5-e33e-48ba-b5c3-e653c73649a8
+START_NODE=2dfadd04-b0ea-4d4e-ad07-ccf386efc404
 # where to move it to
-END_NODE=f273c8c9-7cc6-44a9-af9d-f1524d6077f7
+END_NODE=248f8732-7136-4821-8762-e145ca63ecdc
 
 list-nodes() {
     # a very basic test that creates a volume using the HTTP API
     # and then moves it
-    echo "Check list nodes works"
-    curl \
-        --cacert /etc/flocker/cluster.crt \
-        --cert /etc/flocker/plugin.crt \
-        --key /etc/flocker/plugin.key \
-        https://54.204.134.124:4523/v1/state/nodes
+    curl -s \
+        --cacert $CA_FILE \
+        --cert $CERT_FILE \
+        --key $KEY_FILE \
+        https://$CONTROL_SERVICE:4523/v1/state/nodes
+}
+
+list-volumes-config() {
+    # a very basic test that creates a volume using the HTTP API
+    # and then moves it
+    curl -s \
+        --cacert $CA_FILE \
+        --cert $CERT_FILE \
+        --key $KEY_FILE \
+        https://$CONTROL_SERVICE:4523/v1/configuration/datasets
 }
 
 list-volumes() {
     # a very basic test that creates a volume using the HTTP API
     # and then moves it
-    echo "Check list nodes works"
-    curl \
-        --cacert /etc/flocker/cluster.crt \
-        --cert /etc/flocker/plugin.crt \
-        --key /etc/flocker/plugin.key \
-        https://54.204.134.124:4523/v1/state/datasets
+    curl -s \
+        --cacert $CA_FILE \
+        --cert $CERT_FILE \
+        --key $KEY_FILE \
+        https://$CONTROL_SERVICE:4523/v1/state/datasets
 }
 
 create-volume() {
@@ -43,15 +51,14 @@ create-volume() {
         echo "Please supply a volumename"
         exit 1
     fi
-    echo "Create a new volume"
-    curl \
-        --cacert /etc/flocker/cluster.crt \
-        --cert /etc/flocker/plugin.crt \
-        --key /etc/flocker/plugin.key \
+    curl -s \
+        --cacert $CA_FILE \
+        --cert $CERT_FILE \
+        --key $KEY_FILE \
         -XPOST \
         --header "Content-type: application/json" \
         -d "{\"primary\":\"$START_NODE\",\"maximum_size\": 107374182400, \"metadata\": {\"name\": \"$volumename\"}}" \
-        https://54.204.134.124:4523/v1/configuration/datasets
+        https://$CONTROL_SERVICE:4523/v1/configuration/datasets
 }
 
 move-volume() {
@@ -60,15 +67,14 @@ move-volume() {
         echo "Please supply a volumeid"
         exit 1
     fi
-    echo "Move a volume"
-    curl \
-        --cacert /etc/flocker/cluster.crt \
-        --cert /etc/flocker/plugin.crt \
-        --key /etc/flocker/plugin.key \
+    curl -s \
+        --cacert $CA_FILE \
+        --cert $CERT_FILE \
+        --key $KEY_FILE \
         -XPOST \
         --header "Content-type: application/json" \
         -d "{\"primary\":\"$END_NODE\"}" \
-        https://54.204.134.124:4523/v1/configuration/datasets/$volumeid
+        https://$CONTROL_SERVICE:4523/v1/configuration/datasets/$volumeid
 }
 
 usage() {
@@ -77,7 +83,7 @@ Usage:
 ./test.sh list-nodes
 ./test.sh list-volumes
 ./test.sh create-volume <volumename>
-./test.sh move-volume <volumename>
+./test.sh move-volume <volumeid>
 ./test.sh help
 EOF
     exit 1
@@ -87,6 +93,7 @@ main() {
     case "$1" in
     list-nodes)         shift; list-nodes $@;;
     list-volumes)       shift; list-volumes $@;;
+    list-volumes-config)shift; list-volumes-config $@;;
     create-volume)      shift; create-volume $@;;
     move-volume)        shift; move-volume $@;;
     *)                  usage $@;;
