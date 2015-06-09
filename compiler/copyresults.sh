6a553dbb91f30afd5617ce04fe384a2cb115517f
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-09 10:34:17 +0100

    Copy the binaries out into the ami and vagrant folders

--- compiler/copyresults.sh
+++ compiler/copyresults.sh
@@ -3,13 +3,20 @@
 # copy the results of the compilation out into the amibuilder folder ready for the next step
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-TARGET_DIR="$DIR/../amibuilder/compiled/files"
+AMI_TARGET_DIR="$DIR/../amibuilder/compiled/files"
+VAGRANT_TARGET_DIR="$DIR/../vagrantbuilder/compiled/files"
 DOCKER_NAME="1.7.0-dev-experimental"
 
-mkdir -p $TARGET_DIR
-echo "Copying docker"
-cp $DIR/.build/docker/bundles/$DOCKER_NAME/binary/docker-$DOCKER_NAME $TARGET_DIR/docker
-echo "Copying weave"
-cp $DIR/.build/weave/weave $TARGET_DIR/weave
-echo "Copying docker images"
-cp $DIR/.build/weave/*.tar $TARGET_DIR
+copy-files() {
+    local TARGET_DIR="$1";
+    mkdir -p $TARGET_DIR
+    echo "Copying docker"
+    cp $DIR/.build/docker/bundles/$DOCKER_NAME/binary/docker-$DOCKER_NAME $TARGET_DIR/docker
+    echo "Copying weave"
+    cp $DIR/.build/weave/weave $TARGET_DIR/weave
+    echo "Copying docker images"
+    cp $DIR/.build/weave/*.tar $TARGET_DIR
+}
+
+copy-files $AMI_TARGET_DIR
+copy-files $VAGRANT_TARGET_DIR
