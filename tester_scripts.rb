ec86c8ef375f1fcfa9621a02289da93adf66758a
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-09 19:22:20 +0100

    Cleanup here a bit

--- tester_scripts.rb
+++ tester_scripts.rb
@@ -2,11 +2,10 @@ flocker_zpool_size = '10G'
 flocker_prefix = '/var/opt/flocker'
 
 $install_docker = <<SCRIPT
-#cp /vagrant/.build/docker/bundles/1.7.0-dev/binary/docker-1.7.0-dev /usr/bin/docker
-# this is the docker with DOCKER_EXPERIMENTAL=1
-stop docker
 cp /vagrant/.build/docker/bundles/1.7.0-dev-experimental/binary/docker-1.7.0-dev-experimental /usr/bin/docker
-sudo curl -L -o /usr/bin/weave https://github.com/weaveworks/weave/releases/download/latest_release/weave
+sudo curl --silent --location \
+  --output /usr/bin/weave \
+  https://github.com/weaveworks/weave/releases/download/latest_release/weave
 sudo chmod a+x /usr/bin/weave
 export DEBIAN_FRONTEND=noninteractive
 apt-get -qq update
