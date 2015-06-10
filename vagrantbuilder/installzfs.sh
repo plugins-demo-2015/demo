6f90ac7041cdf0b228940b4de05ede207d7092fd
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-10 14:42:51 +0100

    move the shared install.sh to /tmp not /vagrant

--- vagrantbuilder/installzfs.sh
+++ vagrantbuilder/installzfs.sh
@@ -4,7 +4,13 @@
 export DEBIAN_FRONTEND=noninteractive
 set -e
 
-add-apt-repository -y ppa:zfs-native/stable
-apt-get update
-apt-get -y install libc6-dev
-apt-get -y install zfsutils
+#add-apt-repository -y ppa:zfs-native/stable
+#apt-get update
+#apt-get -y install libc6-dev
+#apt-get -y install zfsutils
+
+
+# used the pinned, compiled version of ZFS
+git clone https://github.com/binocarlos/flocker-base-install /tmp/flocker-base-install
+source /tmp/flocker-base-install/ubuntu/install.sh
+flocker-base-install
