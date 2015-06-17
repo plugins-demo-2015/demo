a0dd587a12d39ece69d335a5a4c879784f04bdd2
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-17 13:20:04 +0100

    Use new VM names

--- vagrantrunner/launch_scope.sh
+++ vagrantrunner/launch_scope.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
-for m in master runner-1 runner-2
-do vagrant ssh $m -c 'sudo /vagrant/bin/scope launch 172.16.70.250 172.16.70.251 172.16.70.252'
+for m in master runner
+do vagrant ssh $m -c 'sudo /vagrant/bin/scope launch 172.16.70.250 172.16.70.251'
 done
