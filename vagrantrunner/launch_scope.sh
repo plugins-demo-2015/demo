fbe184a78788f7ea68c2b7ed2960f293e79b705b
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-17 16:43:39 +0100

    Load scope tarball that is checked-in

--- vagrantrunner/launch_scope.sh
+++ vagrantrunner/launch_scope.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 for m in master runner
-do vagrant ssh $m -c 'sudo /vagrant/bin/scope launch 172.16.70.250 172.16.70.251'
+do vagrant ssh $m -c 'sudo docker load -i /vagrant/bin/scope.tar; sudo /vagrant/bin/scope launch 172.16.70.250 172.16.70.251'
 done
