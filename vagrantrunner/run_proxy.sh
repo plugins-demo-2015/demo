195b9f8ac8de74625c5b9c6786c8b9062f6f5eb0
Ilya Dmitrichenko <errordeveloper@gmail.com>
2015-06-21 10:47:28 -0700

    Use DNS this way for the moment

--- vagrantrunner/run_proxy.sh
+++ vagrantrunner/run_proxy.sh
@@ -2,13 +2,16 @@
 
 set -xe
 
-TARGET_HOSTNAME='hello.weave.local'
 DNS='10.23.11.10'
+
+weave expose
+ip route add $DNS dev weave
+
+TARGET_HOST=$(dig +short hello.weave.local @${DNS})
 
 proxyid=$(docker run -d \
     --name demoproxy \
-    --dns $DNS \
     --net=host \
     -p 80:80 \
     clusterhq/experimental-volumes-gui \
-    python -c "from twisted.python import log; from twisted.internet import reactor; from twisted.web import proxy, server; import sys; log.startLogging(sys.stdout); site = server.Site(proxy.ReverseProxyResource('$TARGET_HOSTNAME', 80, '')); reactor.listenTCP(80, site); reactor.run()")
+    python -c "from twisted.python import log; from twisted.internet import reactor; from twisted.web import proxy, server; import sys; log.startLogging(sys.stdout); site = server.Site(proxy.ReverseProxyResource('$TARGET_HOST', 80, '')); reactor.listenTCP(80, site); reactor.run()")
