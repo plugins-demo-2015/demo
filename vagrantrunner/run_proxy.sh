caa3c222f90ac3bcf5e09aef7f5e507b5c54831e
Luke Marsden <luke@clusterhq.com>
2015-06-21 10:46:27 -0700

    Make twistd use resolver

--- vagrantrunner/run_proxy.sh
+++ vagrantrunner/run_proxy.sh
@@ -11,4 +11,4 @@ proxyid=$(docker run -d \
     --net=host \
     -p 80:80 \
     clusterhq/experimental-volumes-gui \
-    python -c "from twisted.python import log; from twisted.internet import reactor; from twisted.web import proxy, server; import sys; log.startLogging(sys.stdout); site = server.Site(proxy.ReverseProxyResource('$TARGET_HOSTNAME', 80, '')); reactor.listenTCP(80, site); reactor.run()")
+    python -c "from twisted.names.client import createResolver; from twisted.python import log; from twisted.internet import reactor; from twisted.web import proxy, server; import sys; resolver = createResolver(servers=[('$DNS', 53)]); reactor.installResolver(resolver); log.startLogging(sys.stdout); site = server.Site(proxy.ReverseProxyResource('$TARGET_HOSTNAME', 80, '')); reactor.listenTCP(80, site); reactor.run()")
