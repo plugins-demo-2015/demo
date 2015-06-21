c04c918976e7c19868f563e948dba01b51f237f0
Kai Davenport <kaiyadavenport@gmail.com>
2015-06-21 10:55:30 -0700

    point at the baked binaries

--- vagrantrunner/stop_scope.sh
+++ vagrantrunner/stop_scope.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 for m in master runner
-do vagrant ssh $m -c 'sudo /vagrant/bin/scope stop'
+do vagrant ssh $m -c 'sudo /usr/bin/scope stop'
 done
--- vagrantrunner/swarm_manage.sh
+++ vagrantrunner/swarm_manage.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -x
-vagrant ssh master -c '/vagrant/bin/swarm manage --host 0.0.0.0:2378 172.16.70.250:2375,172.16.70.251:2375'
+vagrant ssh master -c '/usr/bin/swarm manage --host 0.0.0.0:2378 172.16.70.250:2375,172.16.70.251:2375'
